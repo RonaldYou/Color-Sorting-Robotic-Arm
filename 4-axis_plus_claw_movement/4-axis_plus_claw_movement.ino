27785cbde0c3159c1fe9c749b301dacf12bc3967
RonaldYou <ronaldrenyou@gmail.com>
2023-08-17 22:58:21 -0400

    simple pick up

--- 4-axis_plus_claw_movement/4-axis_plus_claw_movement.ino
+++ 4-axis_plus_claw_movement/4-axis_plus_claw_movement.ino
@@ -48,84 +48,78 @@ void setup() {
 	base.setSpeed(100);
 
   m1p1.setMaxSpeed(1000);
-	m1p1.setAcceleration(150);
-	m1p1.setSpeed(250);
+	m1p1.setAcceleration(25);
+	m1p1.setSpeed(75);
 
   m1p2.setMaxSpeed(1000);
-	m1p2.setAcceleration(150);
-	m1p2.setSpeed(250);
+	m1p2.setAcceleration(25);
+	m1p2.setSpeed(75);
 
   m2.setMaxSpeed(1000);
 	m2.setAcceleration(25);
-	m2.setSpeed(200);
+	m2.setSpeed(50);
 
   m3.setMaxSpeed(1000);
-	m3.setAcceleration(50);
-	m3.setSpeed(200);
+	m3.setAcceleration(25);
+	m3.setSpeed(75);
 	
   clawServo.write(0);
   clawServo.attach(13);
 
-  //base1.setCurrentPosition(0);
+  base.setCurrentPosition(0);
   m1p1.setCurrentPosition(0);
   m1p2.setCurrentPosition(0);
   m2.setCurrentPosition(0);
-
-  //base1.moveTo(1000);
-  // m1p1.moveTo(-950);
-  // m1p2.moveTo(950);
-  // m2.moveTo(350);
+  m3.setCurrentPosition(0);
 
   Serial.begin(9600);
 }
 
 void loop() {
-  
-  // if(base1.distanceToGo() == 0){
-  //   base1.moveTo(-base1.currentPosition());
-  //   runMotors();
-  // }
-
-  // if(m1p1.distanceToGo() == 0){
-  //   m1p1.moveTo(-m1p1.currentPosition());
-  //   runMotors();
-  // }
-
-  // if(m1p2.distanceToGo() == 0){
-  //   m1p2.moveTo(-m1p2.currentPosition());
-  //   runMotors();
-  // }
-
-  // if(m2.distanceToGo() == 0){
-  //   m2.moveTo(-m2.currentPosition());
-  //   runMotors();
-  // }
   if(phase == 0){
-    m1p1.moveTo(-1900);
-    m1p2.moveTo(1900);
-    //m2.moveTo(350);
-    if(m1p1.distanceToGo() == 0){
+    m1p1.moveTo(-1650);
+    m1p2.moveTo(1650);
+    m2.moveTo(1200);
+    m3.moveTo(1700);
+    if(m1p1.distanceToGo() == 0 && m1p2.distanceToGo() == 0 && m2.distanceToGo() == 0){
       phase = 1;
       for (pos = 0; pos <= 92; pos += 1) { // goes from 0 degrees to 180 degrees
-        // in steps of 1 degree
         clawServo.write(pos);              // tell servo to go to position in variable 'pos'
         delay(15);                       // waits 15ms for the servo to reach the position
       }
     }
     runMotors();
   }
-  else{
-    clawServo.write(pos);              // tell servo to go to position in variable 'pos'
-    m1p1.moveTo(0);
-    m1p2.moveTo(0);
-    //m2.moveTo(0);
+  else if(phase == 1){
+    clawServo.write(pos);             
+    m1p1.moveTo(-1350);
+    m1p2.moveTo(1350);
+    base.moveTo(500);
+    if(m1p1.distanceToGo() == 0 && base.distanceToGo() = 0){
+      phase = 2;
+    }
     runMotors();
   }
+  else if(phase == 2){
+    clawServo.write(pos);
+    m1p1.moveTo(-1650);
+    m1p2.moveTo(1650);
+    if(m1p1.distanceToGo() == 0 && m1p2.distanceToGo() == 0){
+      phase = 3;
+      for (pos = 92; pos => 0; pos--) { // goes from 0 degrees to 180 degrees
+        clawServo.write(pos);              // tell servo to go to position in variable 'pos'
+        delay(15);                       // waits 15ms for the servo to reach the position
+      }
+    }
+    runMotors();
+  }
+
 }
 
 void runMotors(){
-  //base.run();
+  base.run();
   m1p1.run();
   m1p2.run();
-  //m2.run();
+  m2.run();
+  m3.run();
 }
--- Simple_picking_up/Simple_picking_up.ino
+++ Simple_picking_up/Simple_picking_up.ino
@@ -41,30 +41,75 @@ int phase = 0;
 
 
 void setup() {
+
   //unable to find specific from 28bjy-48 datasheet
+	base.setMaxSpeed(1000);
+	base.setAcceleration(50);
+	base.setSpeed(100);
+
+  m1p1.setMaxSpeed(1000);
+	m1p1.setAcceleration(25);
+	m1p1.setSpeed(75);
+
+  m1p2.setMaxSpeed(1000);
+	m1p2.setAcceleration(25);
+	m1p2.setSpeed(75);
+
   m2.setMaxSpeed(1000);
 	m2.setAcceleration(25);
-	m2.setSpeed(75);
+	m2.setSpeed(50);
 
+  m3.setMaxSpeed(1000);
+	m3.setAcceleration(25);
+	m3.setSpeed(75);
+	
+  clawServo.write(0);
+  clawServo.attach(13);
+
+  m1p1.setCurrentPosition(0);
+  m1p2.setCurrentPosition(0);
   m2.setCurrentPosition(0);
+  m3.setCurrentPosition(0);
 
   Serial.begin(9600);
 }
 
 void loop() {
   if(phase == 0){
-    m2.moveTo(1500);
-    if(m2.distanceToGo() == 0){
+    m1p1.moveTo(-1650);
+    m1p2.moveTo(1650);
+    m2.moveTo(1200);
+    m3.moveTo(1700);
+    if(m1p1.distanceToGo() == 0 && m1p2.distanceToGo() == 0 && m2.distanceToGo() == 0){
       phase = 1;
+      for (pos = 0; pos <= 92; pos += 1) { // goes from 0 degrees to 180 degrees
+        clawServo.write(pos);              // tell servo to go to position in variable 'pos'
+        delay(15);                       // waits 15ms for the servo to reach the position
+      }
     }
     runMotors();
   }
-  else{           
+  else if(phase == 1){
+    clawServo.write(pos);              // tell servo to go to position in variable 'pos'
+    m1p1.moveTo(0);
+    m1p2.moveTo(0);
+    if(m1p1.distanceToGo() == 0){
+      phase = 2;
+    }
+    runMotors();
+  }
+  else if(phase == 2){
     m2.moveTo(0);
+    if(m2.distanceToGo() <= 600){
+      m3.moveTo(0);
+    }
     runMotors();
   }
 }
 
 void runMotors(){
+  m1p1.run();
+  m1p2.run();
   m2.run();
+  m3.run();
 }
